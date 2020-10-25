fa752a559b6fb3e2189a220e33403331da2dfee4
ggq <ggq@cal.b7310>
2020-10-25 19:26:20 +0800

    将oracle导入到mongo 增加readme 增加data文件夹

--- prepare/export_orc_to_mongo.py
+++ prepare/export_orc_to_mongo.py
@@ -1,14 +1,38 @@
 from pymongo import MongoClient
+from tqdm import tqdm
 import pandas as pda
 from sqlalchemy import create_engine
 
 client = MongoClient("116.56.140.195",27017)
 db = client.get_database("jiaowu")
 
-connect=create_engine('oracle://CRM_GZ_INST:CRM_GZ_INST@116.56.140.211:1521/B7310oracle')
-sql = "select * from XS201020111"
+connect=create_engine('oracle://JWC_ROOT:JWC_ROOT@116.56.140.211:1521/B7310oracle')
 
-data = pda.read_sql(sql,connect)
-print(data.describe())
+all_table = ["CJB",
+        "JSXXB",
+        "JXRWB",
+        "XSJBXXB",
+        "XSPFB_201020111",
+        "XSPFB_201020112",
+        "XSPFB_201120121",
+        "XSPFB_201120122",
+        "XSPFB_201220131",
+        "XSPFB_201220132",
+        "XSPFB_201320141",
+        "XSPFB_201320142",
+        "XSPFB_201420151",
+        "XSPFB_201420152",
+        "XSPFB_201520161",
+        "XSXKBO",]
+#for item in data.iterrows():
+#    print(item)
 
+def yield_items(data):
+    for item in data.iterrows():
+        yield item[1].to_dict()
 
+for table in tqdm(all_table):
+    col = db.get_collection(table)
+    sql = f"select * from {table}"
+    data = pda.read_sql(sql,connect)
+    col.insert_many(yield_items(data))
