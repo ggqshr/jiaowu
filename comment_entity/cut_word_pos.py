256be08026c214dd2c56c56611d9bb89d5522abb
ggq <ggq@swy.com>
2021-01-29 18:21:51 +0800

    修改能够运行

--- comment_entity/cut_word_pos.py
+++ comment_entity/cut_word_pos.py
@@ -1,4 +1,5 @@
 import fool
+from tqdm import tqdm
 import re
 import sys
 sys.path.append("..")
@@ -14,16 +15,16 @@ lines = filter(None,map(lambda x:x.strip(),content.split("\n")))
 cut_func = fool.pos_cut
 
 process_list = []
-for line in lines:
+for line in tqdm(lines):
     if len(process_list) % 1000 == 0 and len(process_list) != 0:
         res = cut_func(process_list)
         insert_list = []
         for rr,sent in zip(res,process_list):
             this_item = {}
             this_item['sent'] = sent
-            temp_dict = {k:v for k,v in rr}
+            temp_dict = {k.replace(".",""):v for k,v in rr if not v.startswith("w")}
             this_item['words_pos'] = temp_dict
-            this_item['all_words'] = [k for k,v in temp_dict.items() if v.startswith("w")] # 过滤掉标点符号
+            this_item['all_words'] = [k for k,v in temp_dict.items() if not v.startswith("w")] # 过滤掉标点符号
             insert_list.append(this_item)
         cut_word_pos_col.insert_many(insert_list)
         process_list.clear()
