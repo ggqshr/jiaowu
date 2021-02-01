0db623e4b552233123d9beadd02e4eb5db2612ae
ggq <ggq@swy.com>
2021-02-01 11:54:45 +0800

    修改完成pmi计算

--- comment_entity/process_n_word.py
+++ comment_entity/process_n_word.py
@@ -4,6 +4,7 @@
 2.使用pmi计算
 """
 import sys
+from pprint import pprint
 from tqdm import tqdm
 sys.path.append("..")
 from prepare.config import db, r_client
@@ -18,12 +19,15 @@ def get_words_wirte_to_redis():
 
     n_words = list()
     with r_client.pipeline(transaction=False) as p:
-        for item in tqdm(all_items):
+        for index,item in tqdm(enumerate(all_items)):
             content = item.get("words_pos")
             for k, v in content.items():
                 if v.startswith("n"):
                     n_words.append(k)
                     p.sadd("word:"+k, str(item.get("_id")))
+            if index % 1000 == 0 and index != 0:
+                p.execute()
+        p.execute()
     return n_words
 
 
@@ -41,23 +45,23 @@ def get_counter_ge_100(n_words):
     return all_filter_res
 
 
-def cal_pmi(w1, w2, client):
-    w1_len = client.scard("word:%s" % w1)
-    w2_len = client.scard("word:%s" % w2)
-    w1_w2_inter = len(client.sinter("word:%s" % w1, "word:%s" % w2))
-    return w1_w2_inter / (w1_len * w2_len)
+def cal_pmi(w1, w2):
+    w1_len = r_client.scard("word:%s" % w1)
+    w2_len = r_client.scard("word:%s" % w2)
+    w1_w2_inter = len(r_client.sinter("word:%s" % w1, "word:%s" % w2))
+    mul_res = w1_len * w2_len
+    return "%.10f" % (w1_w2_inter / (mul_res + 1))
 
 
 def use_select_cal_pmi(words):
     res_list = []
-    with r_client.pipeline(transaction=False) as p:
-        for word,_ in words:
-            res_list.append((word,cal_pmi("老师",word,p)))
-    print(res_list)
+    for word,_ in words:
+        res_list.append((word,cal_pmi("老师",word)))
+    pprint(list(sorted(res_list,key=lambda x:x[1])))
 
 
 if __name__ == '__main__':
-    # n_words = get_words_wirte_to_redis()
-    # filter_res = get_counter_ge_100(n_words)
+    #n_words = get_words_wirte_to_redis()
+    #filter_res = get_counter_ge_100(n_words)
     filter_res = [i.split(",") for i in filter(None,Path("../data/comment_entity/top_100_n_words.txt").read_text().split("\n"))]
     use_select_cal_pmi(filter_res)
