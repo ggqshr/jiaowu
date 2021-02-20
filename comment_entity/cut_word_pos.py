aab65c667eb31fed4e2189b048b304d7418c6315
ggqshr <ggq18663278150@sina.com>
2021-02-20 22:42:52 +0800

    更新依赖文件，并修改cut_word改成分布式

--- comment_entity/cut_word_pos.py
+++ comment_entity/cut_word_pos.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import re
 import sys
 sys.path.append("..")
-from prepare.config import db
+from prepare.config import db,r_client
 from pathlib import Path
 from io import StringIO
 from tempfile import NamedTemporaryFile
@@ -16,9 +16,11 @@ ltp = LTP(path="/home/songwenyu/data/base.tgz")
 
 cut_word_pos_col = db.get_collection("pos_words")
 
-data_path = Path("../data/all_text.txt")
-content = data_path.read_text(encoding="utf-8")
-lines = list(filter(None,map(lambda x:x.strip(),content.split("\n")))) 
+def get_line():
+    with r_client.pipeline(transaction=False) as p:
+        while p.llen("lines").execute() != 0:
+            yield p.lpop("lines").execute()[0]
+lines = get_line()
 
 teacher_info = db.get_collection("JSXXB")
 all_teacher_name = [i.get("xm") for i in teacher_info.find({},{"xm":True,"_id":False}) if len(bytes(i.get("xm")[0],"utf-8")) != 1] # 只取中文的老师
@@ -45,11 +47,15 @@ def cut_func(sent_list):
     seg,hidden = ltp.seg(sent_list)
     pos = ltp.pos(hidden)
     dep = ltp.dep(hidden)
-    for s,p,d in zip(seg,pos,dep):
-        yield zip(s,p),d
+    sdp = ltp.sdp(hidden)
+    for s,p,d,ss in zip(seg,pos,dep,sdp):
+        yield zip(s,p),d,ss
 process_list = []
 
 def sub_match(item):
+    """
+    将 李老师 这种词语替换成 老师 
+    """
     k,v = item
     if re.match(all_first_name_to_sub_reg,k):
         return "老师",v
@@ -60,13 +66,17 @@ def process(res,ll):
     for rr,sent in zip(res,ll):
         pos_res = rr[0]
         dep_res = rr[1]
+        sdp_res = rr[2]
         this_item = {}
         this_item['sent'] = sent
-        temp_dict = [(k.replace(".","").replace("$",""),v) for k,v in pos_res if v!="wp"]
-        temp_dict = list(map(sub_match,temp_dict))
-        this_item['words_pos'] = temp_dict
-        this_item['all_words'] = [k for k,v in temp_dict if not v.startswith("w")] # 过滤掉标点符号
+        this_item['origin_pos'] = [(k.replace(".","").replace("$",""),v) for k,v in pos_res] # 原始的分词词性标注结果，包含标点符号
+        temp_dict = [(k.replace(".","").replace("$",""),v) for k,v in pos_res if v!="wp"] 
+        # temp_dict = list(map(sub_match,temp_dict))
+        this_item['filter_pos'] = temp_dict # 去除标点符号的标注结果
+        this_item['all_words_origin'] = [k for k,v in this_item['origin_pos']] # 原始的词语序列
+        this_item['all_words_filter'] = [k for k,v in temp_dict if v != "wp"] # 过滤掉标点符号
         this_item["dep"] = dep_res
+        this_item['sdp'] = sdp_res
         insert_list.append(this_item)
     return insert_list
 
