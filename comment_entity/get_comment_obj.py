e022abb42f05d62f8cb15be2f7b30dc8dc51398c
ggqshr <ggq18663278150@sina.com>
2021-02-21 11:36:12 +0800

    完成了table1的1-7规则

---
+++ comment_entity/get_comment_obj.py
@@ -0,0 +1,83 @@
+"""
+使用规则的方法来获得评论中的评价对象的程序，采用的是
+[1].	张璞, 李逍与刘畅, 基于规则的评价搭配抽取方法. 计算机工程, 2019. 45(08): 第217-223页.
+的方法
+"""
+from typing import List
+import re
+import sys
+sys.path.append("..")
+from prepare.config import r_client, db
+
+pos_col = db.get_collection("pos_words")
+
+all_items = pos_col.find(
+    {}, {"dep": True, "origin_pos": True, "_id": True,"sent":True}, no_cursor_timeout=True)
+
+
+def get_relationship(words_pos_list, start, end):
+    start_word, start_pos = words_pos_list[start-1]
+    if end == 0:
+        end_word,end_pos = "None","ROOT"
+    else:
+        end_word, end_pos = words_pos_list[end-1]
+    return start_pos, end_pos, start_word, end_word
+
+
+def table_4_rule_1(start_pos, end_pos, rel):
+    if rel == "SBV" and start_pos == "n" and end_pos == "a":
+        return True
+    return False
+
+
+def table_4_rule_2(start_pos, end_pos, rel):
+    if rel == "SBV" and start_pos == "n" and (end_pos == "d" or end_pos == "i"):
+        return True
+    return False
+
+
+def table_4_rule_3(start_pos, end_pos, rel):
+    if rel == "SBV" and start_pos == "n" and end_pos == "v":
+        return True
+    return False
+
+
+def table_4_rule_4(start_pos, end_pos, rel):
+    if rel == "SBV" and start_pos == "v" and end_pos == "a":
+        return True
+    return False
+
+
+def table_4_rule_5(start_pos, end_pos, rel):
+    if rel == "SBV" and start_pos == "v" and end_pos == "v":
+        return True
+    return False
+
+
+def table_4_rule_6(start_pos, end_pos, rel):
+    if rel == "CMP" and start_pos == "v" and end_pos == "a":
+        return True
+    return False
+
+
+def table_4_rule_7(start_pos, end_pos, rel):
+    if rel == "CMP" and start_pos == "v" and end_pos == "d":
+        return True
+    return False
+
+all_table1_rule = [eval("table_4_rule_%s" % i) for i in range(1,8)]
+
+for item in all_items:
+    _id = item.get("_id")
+    dep = item.get("dep")
+    words_origin = item.get("origin_pos")
+    sent = item.get("sent")
+    this_res = []
+    # table 4 rule 1
+    for dd in dep:
+        start,end,rel = dd
+        start_pos,end_pos,start_word,end_word = get_relationship(words_origin,start,end)
+        for rule in all_table1_rule:
+            if rule(start_pos,end_pos,rel):
+                this_res.append((start_word,end_word))
+    print("sent : %s\n comment_obj: %s" % (sent,this_res) )
