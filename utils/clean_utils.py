17813ff8e1ae989474a6e188f3dd21cccbec0529
ggqshr <ggq18663278150@sina.com>
2021-02-25 21:38:01 +0800

    fix

--- utils/clean_utils.py
+++ utils/clean_utils.py
@@ -8,6 +8,11 @@ from prepare.config import db
 from pathlib import Path
 from collections import defaultdict
 
+all_stop_words = list(filter(None,Path(__file__).parent.joinpath("../data/stopwords.txt").read_text(encoding="utf-8").split("\n")))
+filter_map = defaultdict(bool)
+for word in all_stop_words:
+    filter_map[word] = True
+
 class SubFirstNameMatch:
     """
     用来将 李老师 替换成 老师
@@ -18,7 +23,7 @@ class SubFirstNameMatch:
     def __init__(self,):
         pass
 
-    def sub_match(self,item):
+    def sub_match(self,word):
         """
         将 李老师 这种词语替换成 老师 
         """
@@ -29,21 +34,9 @@ class SubFirstNameMatch:
             all_first_name = [i[0] for i in all_teacher_name]
             all_first_name = set(filter(lambda x:not ('a' <= x <= 'z'),map(lambda x:x.lower(),all_first_name)))
             self.all_first_name_to_sub_reg = "(%s)老师" % "|".join(all_first_name)
-        k,v = item
-        if re.match(self.all_first_name_to_sub_reg,k):
-            return "老师",v
-        return k,v
+        if re.match(self.all_first_name_to_sub_reg,word):
+            return "老师"
+        return word
 
-class FilterStopWord:
-    """
-    去重停用词
-    """
-    all_stop_words = list(filter(None,Path(__file__).parent.joinpath("../data/stopwords.txt").read_text(encoding="utf-8").split("\n")))
-    filter_map = None
-
-    def filter_stop(self,word):
-        if self.filter_map is None:
-            self.filter_map = defaultdict(bool)
-            for ww in self.all_stop_words:
-                self.filter_map[ww] = True
-        return self.filter_map[word]
+def filter_stop_words(word):
+    return filter_map[word]
